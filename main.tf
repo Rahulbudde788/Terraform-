1f1fa8eeaadeb4567cf0adeae5154af73ab6364f
Rahulbudde788 <rahulbudde788@gmail.com>
2024-10-23 20:06:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ count = 4
 ami = "ami-0e0e417dfa2028266"
 instance_type = "t2.medium"
 key_name = "rahul2"
-vpc_security_group_ids = [""]
+vpc_security_group_ids = ["sg-0bb3f9963116dfc7c"]
 tags = {
 Name = var.instance_names[count.index]
 }

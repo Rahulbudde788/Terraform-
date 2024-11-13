13f7036ba2df9568de40798b36dbd888064e3cbb
Rahulbudde788 <rahulbudde788@gmail.com>
2024-11-13 10:09:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,16 +3,6 @@ region = "ap-south-1"
 }
 
 resource "aws_instance" "one" {
-count = 4
-ami = "ami-0e0e417dfa2028266"
-instance_type = "t2.medium"
-key_name = "rahul2"
-vpc_security_group_ids = ["sg-0bb3f9963116dfc7c"]
-tags = {
-Name = var.instance_names[count.index]
-}
-}
-
-variable "instance_names" {
-default = ["jenkins", "nexus", "tomcat-1", "tomcat-2"]
+ami = "ami-03753afda9b8ba740"
+instance_type = "t2.large"
 }
